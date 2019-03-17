58cb36133c6f1a6e1a1159cdf1d96f92e30e84d7
dazingwill <spumirant@aliyun.com>
2019-03-16 21:26:29 -0700

    update IBM API authentication (from username/password to API key)

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -1198,27 +1198,24 @@ class Recognizer(AudioSource):
             raise UnknownValueError()
         return result['Disambiguation']['ChoiceData'][0]['Transcription']
 
-    def recognize_ibm(self, audio_data, username, password, language="en-US", show_all=False):
+    def recognize_ibm(self, audio_data, key, region="stream", language="en-US", show_all=False):
         """
-        Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the IBM Speech to Text API.
+        The IBM Speech to Text API key are specified by ``key``. Unfortunately, these are not available without `signing up for an account <https://console.bluemix.net/registration/>`__. Once logged into the Bluemix console, follow the instructions for `creating an IBM Watson service instance <https://cloud.ibm.com/docs/services/watson?topic=watson-creating-credentials>`__, where the Watson service is "Speech To Text". IBM Speech to Text API keys are mixed-case alphanumeric strings.
 
-        The IBM Speech to Text username and password are specified by ``username`` and ``password``, respectively. Unfortunately, these are not available without `signing up for an account <https://console.ng.bluemix.net/registration/>`__. Once logged into the Bluemix console, follow the instructions for `creating an IBM Watson service instance <https://www.ibm.com/watson/developercloud/doc/getting_started/gs-credentials.shtml>`__, where the Watson service is "Speech To Text". IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX, while passwords are mixed-case alphanumeric strings.
+        The recognition language is determined by ``language``, an RFC5646 language tag with a dialect like ``"en-US"`` (US English) or ``"zh-CN"`` (Mandarin Chinese), defaulting to US English. The supported language values are listed under the ``model`` parameter of the `audio recognition API documentation <https://cloud.ibm.com/apidocs/speech-to-text>`__, in the form ``LANGUAGE_BroadbandModel``, where ``LANGUAGE`` is the language value.
 
-        The recognition language is determined by ``language``, an RFC5646 language tag with a dialect like ``"en-US"`` (US English) or ``"zh-CN"`` (Mandarin Chinese), defaulting to US English. The supported language values are listed under the ``model`` parameter of the `audio recognition API documentation <https://www.ibm.com/watson/developercloud/speech-to-text/api/v1/#sessionless_methods>`__, in the form ``LANGUAGE_BroadbandModel``, where ``LANGUAGE`` is the language value.
-
-        Returns the most likely transcription if ``show_all`` is false (the default). Otherwise, returns the `raw API response <https://www.ibm.com/watson/developercloud/speech-to-text/api/v1/#sessionless_methods>`__ as a JSON dictionary.
+        Returns the most likely transcription if ``show_all`` is false (the default). Otherwise, returns the `raw API response <https://cloud.ibm.com/docs/services/speech-to-text?topic=speech-to-text-http>`__ as a JSON dictionary.
 
         Raises a ``speech_recognition.UnknownValueError`` exception if the speech is unintelligible. Raises a ``speech_recognition.RequestError`` exception if the speech recognition operation failed, if the key isn't valid, or if there is no internet connection.
         """
         assert isinstance(audio_data, AudioData), "Data must be audio data"
-        assert isinstance(username, str), "``username`` must be a string"
-        assert isinstance(password, str), "``password`` must be a string"
+        assert isinstance(key, str), "``key`` must be a string"
 
         flac_data = audio_data.get_flac_data(
             convert_rate=None if audio_data.sample_rate >= 16000 else 16000,  # audio samples should be at least 16 kHz
             convert_width=None if audio_data.sample_width >= 2 else 2  # audio samples should be at least 16-bit
         )
-        url = "https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?{}".format(urlencode({
+        url = "https://{}.watsonplatform.net/speech-to-text/api/v1/recognize?{}".format(region, urlencode({
             "profanity_filter": "false",
             "model": "{}_BroadbandModel".format(language),
             "inactivity_timeout": -1,  # don't stop recognizing when the audio stream activity stops
@@ -1227,7 +1224,7 @@ class Recognizer(AudioSource):
             "Content-Type": "audio/x-flac",
             "X-Watson-Learning-Opt-Out": "true",  # prevent requests from being logged, for improved privacy
         })
-        authorization_value = base64.standard_b64encode("{}:{}".format(username, password).encode("utf-8")).decode("utf-8")
+        authorization_value = base64.standard_b64encode("{}:{}".format("apikey", key).encode("utf-8")).decode("utf-8")
         request.add_header("Authorization", "Basic {}".format(authorization_value))
         try:
             response = urlopen(request, timeout=self.operation_timeout)
@@ -1245,7 +1242,8 @@ class Recognizer(AudioSource):
 
         transcription = []
         for utterance in result["results"]:
-            if "alternatives" not in utterance: raise UnknownValueError()
+            if "alternatives" not in utterance:
+                raise UnknownValueError()
             for hypothesis in utterance["alternatives"]:
                 if "transcript" in hypothesis:
                     transcription.append(hypothesis["transcript"])
--- tests/test_recognition.py
+++ tests/test_recognition.py
@@ -63,23 +63,26 @@ class TestRecognition(unittest.TestCase):
         with sr.AudioFile(self.AUDIO_FILE_EN) as source: audio = r.record(source)
         self.assertEqual(r.recognize_houndify(audio, client_id=os.environ["HOUNDIFY_CLIENT_ID"], client_key=os.environ["HOUNDIFY_CLIENT_KEY"]), "one two three")
 
-    @unittest.skipUnless("IBM_USERNAME" in os.environ and "IBM_PASSWORD" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
+    @unittest.skipUnless("IBM_KEY" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
     def test_ibm_english(self):
         r = sr.Recognizer()
         with sr.AudioFile(self.AUDIO_FILE_EN) as source: audio = r.record(source)
-        self.assertEqual(r.recognize_ibm(audio, username=os.environ["IBM_USERNAME"], password=os.environ["IBM_PASSWORD"]), "one two three ")
+        region = os.environ["IBM_REGION"] if "IBM_REGION" in os.environ else "stream"
+        self.assertEqual(r.recognize_ibm(audio, key=os.environ["IBM_KEY"], region=region), "one two three ")
 
-    @unittest.skipUnless("IBM_USERNAME" in os.environ and "IBM_PASSWORD" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
+    @unittest.skipUnless("IBM_KEY" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
     def test_ibm_french(self):
         r = sr.Recognizer()
         with sr.AudioFile(self.AUDIO_FILE_FR) as source: audio = r.record(source)
-        self.assertEqual(r.recognize_ibm(audio, username=os.environ["IBM_USERNAME"], password=os.environ["IBM_PASSWORD"], language="fr-FR"), u"si la dictée numéro un ")
+        region = os.environ["IBM_REGION"] if "IBM_REGION" in os.environ else "stream"
+        self.assertEqual(r.recognize_ibm(audio, key=os.environ["IBM_KEY"], region=region, language="fr-FR"), u"si la dictée numéro un ")
 
-    @unittest.skipUnless("IBM_USERNAME" in os.environ and "IBM_PASSWORD" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
+    @unittest.skipUnless("IBM_KEY" in os.environ, "requires IBM Speech to Text username and password to be specified in IBM_USERNAME and IBM_PASSWORD environment variables")
     def test_ibm_chinese(self):
         r = sr.Recognizer()
         with sr.AudioFile(self.AUDIO_FILE_ZH) as source: audio = r.record(source)
-        self.assertEqual(r.recognize_ibm(audio, username=os.environ["IBM_USERNAME"], password=os.environ["IBM_PASSWORD"], language="zh-CN"), u"砸 自己 的 脚 ")
+        region = os.environ["IBM_REGION"] if "IBM_REGION" in os.environ else "stream"
+        self.assertEqual(r.recognize_ibm(audio, key=os.environ["IBM_KEY"], region=region, language="zh-CN"), u"砸 自己 的 脚 ")
 
 
 if __name__ == "__main__":
